ff1763939e1e33f210dfda3933c6353462350a45
Viacheslav <salnikov90@yandex.ru>
2023-11-30 23:22:08 +0500

    add Examples_Condition ifElse

---
+++ Examples005_ConditionifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ksenia")
+{
+    Console.WriteLine("Мы так рады тебя видеть,Ksenia!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
